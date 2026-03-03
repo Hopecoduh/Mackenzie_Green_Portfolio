37a12966947be5f513008386d7dcd3cd1f7a886c
Mac <33965646+SodaDakoda@users.noreply.github.com>
2026-03-03 09:02:19 -0800

    initial build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Capabilities } from './pages/capabilities/capabilities';
+import { Projects } from './pages/projects/projects';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'capabilities', component: Capabilities },
+  { path: 'projects', component: Projects },
+  { path: 'contact', component: Contact },
+];
